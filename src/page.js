23c4b245c1aaa6c34217306b89c185875458e0c8
trox115 <tofernandes67@gmail.com>
2019-11-13 22:15:14 +0000

    Menu elements done

--- src/page.js
+++ src/page.js
@@ -7,13 +7,41 @@ function PageContent() {
 
     let text = document.createElement("div");
     text.classList.add("text");
+    let nav = document.createElement("nav");
+    let ul = document.createElement("ul");
+
+    let liHome = document.createElement("li");
+    let aHome = document.createElement("a");
+    aHome.id = "home";
+    aHome.textContent = "Home";
+    liHome.appendChild(aHome);
+    ul.appendChild(liHome);
+
+
+    let liMenu = document.createElement("li");
+    let aMenu = document.createElement("a");
+    aMenu.id = "menu";
+    aMenu.textContent = "Menu";
+    liMenu.appendChild(aMenu);
+    ul.appendChild(liMenu);
+
+    let liContact = document.createElement("li");
+    let aContact = document.createElement("a");
+    aContact.id = "contact";
+    aContact.textContent = "Contact";
+    liContact.appendChild(aContact);
+    ul.appendChild(liContact);
+
+      nav.appendChild(ul);
+
+
     let title = document.createElement("h1");
     title.textContent = 'This is the restaurant page';
 
 
         let subtitle = document.createElement("p");
         subtitle.textContent = 'Here you find the best food. We are passionate about what we do. And we will prepare you the best food that ever entered in your mouth ';
-
+    text.appendChild(nav);
     text.appendChild(title);
     text.appendChild(subtitle);
 
